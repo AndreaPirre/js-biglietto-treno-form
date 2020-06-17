7dde33a9072f1f0b77a1a2308db9b91da420138e
Andrea <andreapirrello1990@gmail.com>
2020-06-17 15:03:55 +0200

    corretta parte di calcolo del costo biglietto

--- js/script.js
+++ js/script.js
@@ -4,9 +4,6 @@ var annullaBtn = document.getElementById('annullaBiglietto');
 creaBtn.addEventListener("click", function () {
 
     var costoBiglietto;
-    var costoBase = km * 0.21;
-    var costoMinorenni = costoBase - (costoBase * 20 / 100);
-    var costoOver = costoBase - (costoBase * 40 / 100);
     var prezzoKm = 0.21;
     var nome = document.getElementById('nome').value;
     var km = document.getElementById('km').value;
@@ -17,11 +14,11 @@ creaBtn.addEventListener("click", function () {
     var maxNumeroTreno =100000;
 
     if (categoria == "minorenne") {
-        var costoBiglietto = costoMinorenni.toFixed(2) + " euro" + " (hai meno di 18 anni, è già stato applicato uno sconto del 20%)"  ;
+        var costoBiglietto = km * prezzoKm - 20/100 * (km * prezzoKm).toFixed(2) + " euro" + " (hai meno di 18 anni, è già stato applicato uno sconto del 20%)"  ;
     }else if (categoria == "over65") {
-        var costoBiglietto = costoOver.toFixed(2) + " euro" + " (hai più di 65 anni, è già stato applicato uno sconto del 40%)";
+        var costoBiglietto = km * prezzoKm - 40/100 * (km * prezzoKm).toFixed(2) + " euro" + " (hai più di 65 anni, è già stato applicato uno sconto del 40%)";
     }else {
-        var costoBiglietto = costoBase.toFixed(2) + " euro" + " (è stata applicata la tariffa base)";;
+        var costoBiglietto = km * prezzoKm.toFixed(2) + " euro" + " (è stata applicata la tariffa base)";
     }
 
     var numeroTreno = Math.floor(Math.random()*(maxNumeroTreno - minNumeroTreno + 1)) + minNumeroTreno;
